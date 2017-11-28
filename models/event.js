12676b1b0b621d9986330ff3c6e818c63254e69a
Devin <djoleary07@gmail.com>
2017-11-28 16:20:59 -0500

    initial commit

---
+++ models/event.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+let EventSchema = new mongoose.Schema({
+  title:{
+    type: String,
+    required: true
+  },
+  date: {
+    type: String
+  },
+  time: {
+    type: String
+  },
+  category: {
+    type: String
+  },
+  description: {
+    type: String
+  }
+});
+
+let Event = mongoose.model('Event', EventSchema);
+
+module.exports = {Event}
---
+++ routes/event.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const {mongoose} = require('../db/mongoose');
+const {Event} = require('../models/event');
+
+router.post('/', (req, res) => {
+  const event = new Event({
+    title:req.body.title,
+    date:req.body.date,
+    time: req.body.time,
+    category:req.body.category,
+    description:req.body.description
+  });
+  event.save().then((doc) => {
+    res.send({doc});
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
+});
+
+router.get('/:category', (req,res) => {
+  const category = req.params.category;
+  Event.find({category}).then((docs) => {
+    if(!docs){
+      res.status(404).send();
+    }
+    res.send({docs})
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
+});
+
+module.exports = router;
