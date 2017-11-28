2fc9e27dee94a2ed623c9965867456dec6939827
Devin <djoleary07@gmail.com>
2017-11-28 17:14:26 -0500

    cors support

--- app.js
+++ app.js
@@ -18,12 +18,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //server client static files
 app.use(express.static(`${__dirname}/client/build`));
 app.use('/api/event', eventRoutes);
 app.get('*', (req, res) => {
   res.sendFile(`${__dirname}/client/build/index.html`);
 });
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
